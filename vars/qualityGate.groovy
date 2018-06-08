746a0c7ee6ef98acd6fbd507e05b5fa7ea1b7258
acamu <acamu@users.noreply.github.com>
2018-06-08 17:55:21 +0200

    Create qualityGate.groovy

---
+++ vars/qualityGate.groovy
@@ -0,0 +1,14 @@
+import groovy.json.*
+ 
+boolean checkQualityGateConformity(String test, String item) {
+  boolean returnValue = true
+  def jsonSlurper = new JsonSlurper()
+  def data = jsonSlurper.parseText(test)
+  def listdata = data.CONFORMITE
+  for (itemdata in listdata) { 
+        if (itemdata."$item" == "NOK") { 
+                     return false 
+        } 
+  }     
+return returnValue; 
+} 
