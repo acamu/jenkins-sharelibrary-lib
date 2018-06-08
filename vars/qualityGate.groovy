1048315694e62aab87ad319e730d37591cca6f17
acamu <acamu@users.noreply.github.com>
2018-06-08 20:17:57 +0200

    Update qualityGate.groovy

--- vars/qualityGate.groovy
+++ vars/qualityGate.groovy
@@ -1,15 +1,1 @@
-import groovy.json.*
- 
-@NonCPS
-boolean checkQualityGateConformity(String test, String item) {
-  boolean returnValue = true
-  def jsonSlurper = new JsonSlurper()
-  def data = jsonSlurper.parseText(test)
-  def listdata = data.CONFORMITE
-  for (itemdata in listdata) { 
-        if (itemdata."$item" == "NOK") { 
-                     return false 
-        } 
-  }     
-return returnValue; 
-} 
+
